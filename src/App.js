36f4ef51de904eb03273ecb2f5e718b3970bfea6
chenyongping <chenyongping001@gmail.com>
2021-08-03 15:39:52 +0800

    lifting the state up

--- src/App.js
+++ src/App.js
@@ -1,17 +1,51 @@
-import React from 'react';
-import './App.css';
-import Navbar from './components/navbar';
-import Counters from './components/counters';
+import React, { Component } from "react";
+import "./App.css";
+import Navbar from "./components/navbar";
+import Counters from "./components/counters";
 
-function App() {
-  return (
-    <React.Fragment>
-      <Navbar />
-      <main className="container">
-        <Counters />
-      </main>
-    </React.Fragment>
-  );
+class App extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 4 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 },
+    ],
+  };
+  handleDelete = (counter) => {
+    const counters = this.state.counters.filter((c) => c.id !== counter.id);
+    this.setState({ counters });
+  };
+  handleIncrement = (counter) => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index].value++;
+    this.setState({ counters });
+  };
+  handleReset = () => {
+    const counters = this.state.counters.map((c) => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <Navbar
+          totalCounters={this.state.counters.filter((c) => c.value > 0).length}
+        />
+        <main className="container">
+          <Counters
+            counters={this.state.counters}
+            onIncrement={this.handleIncrement}
+            onReset={this.handleReset}
+            onDelete={this.handleDelete}
+          />
+        </main>
+      </React.Fragment>
+    );
+  }
 }
-
 export default App;
